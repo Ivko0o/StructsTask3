26564764e4bd25185560b410185fb81f0f223cdf
Ivko0o <IvelinVasilev91@gmail.com>
2025-03-10 11:00:17 +0200

    Improved program version

--- StructsTask3.cpp
+++ StructsTask3.cpp
@@ -19,12 +19,34 @@ struct Fruit
 За целта да се дефинират подходящи функции. Постарайте се да гарантирате коректност на данните.
 */
 
+/*
+Task 3. The Fruit structure defining a fruit is defined as follows:
+struct Fruit
+{
+  char name[32]; // fruit name
+  color cl; // fruit color
+  yn annual; // annual fruit
+  yn perennial; // perennial fruit
+  yn tropical; // tropical fruit
+};
+where:
+- color is an enumerated type for color (e.g. brown, green, orange, red, yellow, etc.).
+- yn is an enumerated type with two values Yes and No
+Write a program that:
+(a) creates a one-dimensional array of pointers to structures specifying fruits and reads the data from the keyboard;
+b) sorts by name, color, and fruiting duration the fruits specified by the array;
+c) outputs the data for the fruits specified by the array after sorting them;
+d) outputs the perennial, tropical fruits of a given color, sorted in descending order by fruit name.
+For this purpose, define appropriate functions. Try to ensure correctness of the data.
+*/
+
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 enum color {
-    Brown,
+    Brown = 1,
     Green,
     Orange,
     Red,
@@ -47,6 +69,12 @@ struct Fruit {
 void CreateFruits(Fruit* arr[], const size_t& s);
 string ColorToString(color a);
 string TypeCycleToString(Yn a);
+bool CompareFruitsAscending(Fruit* a, Fruit* b);
+bool CompareFruitsDescending(Fruit* a, Fruit* b);
+void SortFruitsAscending(Fruit* arr[], int size);
+void SortFruitsDescending(Fruit* arr[], int size);
+void PrintFruits(Fruit* arr[], int size);
+void PrintTropicalFruits(Fruit* arr[], int size);
 
 
 int main()
@@ -55,19 +83,28 @@ int main()
 
     cout << "Enter how many Fruits do you want to create: ";
     cin >> size;
-    Fruit** fruitsArray = new Fruit*[size];
-    CreateFruits(fruitsArray, size);
 
-    for (size_t i = 0; i < size; ++i) {
-        cout << "Name: " << fruitsArray[i]->name << "\n";
-        cout << "Color: " << ColorToString(fruitsArray[i]->cl) << "\n";
-        cout << "Annual: " << TypeCycleToString(fruitsArray[i]->annual) << "\n";
-        cout << "Perennial: " << TypeCycleToString(fruitsArray[i]->perennial) << "\n";
-        cout << "Tropical: " << TypeCycleToString(fruitsArray[i]->tropical) << "\n";
-        cout << "\n";
+    //Allocates memory for the fruits
+    Fruit** fruitsArray = new(nothrow) Fruit*[size];
+    if (!fruitsArray) {
+        cerr << "Couldn`t allocate memory!\n";
+        return -1;
     }
 
+    Fruit** tempFruitsArray = new Fruit*[size];
+ 
+    CreateFruits(fruitsArray, size);
+    copy(fruitsArray, fruitsArray + size, tempFruitsArray);
+    SortFruitsAscending(fruitsArray,size);
+    PrintFruits(fruitsArray,size);
+    PrintTropicalFruits(tempFruitsArray, size);
 
+
+    //Release the allocated memory
+    for (size_t i = 0; i < size; ++i) {
+        delete fruitsArray[i];
+    }
+    delete[] fruitsArray;
 }
 
  void CreateFruits(Fruit* arr[], const size_t& s) {
@@ -104,16 +141,16 @@ int main()
 
          }
 
-         if (numColor - 1 == color::Brown) {
+         if (numColor == color::Brown) {
              arr[i]->cl = color::Brown;
          }
-         else if (numColor - 1 == color::Green) {
+         else if (numColor == color::Green) {
              arr[i]->cl = color::Green;
          }
-         else if (numColor - 1 == color::Orange) {
+         else if (numColor == color::Orange) {
              arr[i]->cl = color::Orange;
          }
-         else if (numColor - 1 == color::Red) {
+         else if (numColor == color::Red) {
              arr[i]->cl = color::Red;
          }
          else {
@@ -156,7 +193,7 @@ int main()
              cout << "\nYou have to choose between option 1 or 2!\n";
 
          }
-         if (tropicalType == 0) {
+         if (tropicalType - 1 == 0) {
              arr[i]->tropical = Yn::Yes;
          }else{
              arr[i]->tropical = Yn::No;
@@ -164,6 +201,97 @@ int main()
      }
 
 }
+
+ 
+ void SortFruitsAscending(Fruit* arr[], int size) {    
+     sort(arr, arr + size, CompareFruitsAscending);
+ }
+
+ void SortFruitsDescending(Fruit* arr[], int size) {
+     sort(arr, arr + size, CompareFruitsDescending);
+ }
+
+ //Compare fruits based on name, color and TypeCycle in ascending way
+ bool CompareFruitsAscending(Fruit* a, Fruit* b) {
+     if (strcmp(a->name, b->name) == 0) {
+         if (a->cl == b->cl) {
+             return a->annual < b->annual;
+         }
+
+         return a->cl < b->cl;
+     }
+
+     return strcmp(a->name, b->name) < 0;
+ }
+
+ bool CompareFruitsDescending(Fruit* a, Fruit* b) {
+     return a->name > b->name;
+ }
+
+ void PrintTropicalFruits(Fruit* arr[], int size) {
+     size_t  option = 0;
+     cout << "\nChoose the color of the tropical fruits that you want to print\n";
+     cout << "1. Brown\n";
+     cout << "2. Green\n";
+     cout << "3. Orange\n";
+     cout << "4. Red\n";
+     cout << "5. Yellow\n";
+
+     while (true) {
+         cin >> option;
+         if (option >= 1 && option <= 5) {
+             break;
+         }
+         cout << "\nYou have to choose between options from 1 to 5!\n";
+     }
+
+     //Create a temporary array to store only the tropical fruits
+     Fruit** tropicalFruits = new Fruit* [size];
+     int tropicalFruitsCount = 0;
+
+     //Store the tropical fruits in the new array
+     for (size_t i = 0; i < size; ++i) {
+         if (arr[i]->cl == option && arr[i]->tropical == Yn::Yes) {
+             tropicalFruits[tropicalFruitsCount] = arr[i];
+             tropicalFruitsCount++;
+         }
+     }
+
+     //Check if there are any tropical fruits at all
+     if (tropicalFruitsCount == 0) {
+         cout << "\nThere are no tropical fruits!\n";
+         delete[] tropicalFruits;
+             return;
+     }
+
+     //Sorts the tropical fruits in descending order
+     SortFruitsDescending(tropicalFruits, tropicalFruitsCount);
+
+     cout << "Tropical Fruits: \n";
+     for (size_t i = 0; i < tropicalFruitsCount; ++i) {
+             cout << "Name: " << tropicalFruits[i]->name << "\n";
+             cout << "Color: " << ColorToString(tropicalFruits[i]->cl) << "\n";
+             cout << "Annual: " << TypeCycleToString(tropicalFruits[i]->annual) << "\n";
+             cout << "Perennial: " << TypeCycleToString(tropicalFruits[i]->perennial) << "\n";
+             cout << "Tropical: " << TypeCycleToString(tropicalFruits[i]->tropical) << "\n";
+             cout << "\n";
+         }
+
+     delete[] tropicalFruits;
+
+ }
+
+ void PrintFruits(Fruit* arr[], int size) {
+     for (size_t i = 0; i < size; ++i) {
+         cout << "Name: " << arr[i]->name << "\n";
+         cout << "Color: " << ColorToString(arr[i]->cl) << "\n";
+         cout << "Annual: " << TypeCycleToString(arr[i]->annual) << "\n";
+         cout << "Perennial: " << TypeCycleToString(arr[i]->perennial) << "\n";
+         cout << "Tropical: " << TypeCycleToString(arr[i]->tropical) << "\n";
+         cout << "\n";
+     }
+ }
+
 
  string ColorToString(color a) {
      switch (a) {
